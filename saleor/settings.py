002624b105712672bf0c96306ee7d5d88f290ff8
vantrong291 <vantrong291@gmail.com>
2019-10-01 13:54:58 +0700

    user one db

--- saleor/settings.py
+++ saleor/settings.py
@@ -55,7 +55,8 @@ CACHES = {"default": django_cache_url.config()}
 
 DATABASES = {
     "default": dj_database_url.config(
-        default="postgres://saleor:saleor@localhost:5432/saleor", conn_max_age=600
+        default="postgres://saleor:saleor@112.137.131.12:5432/saleor", conn_max_age=600
+        # default="postgres://saleor:saleor@localhost:5432/saleor", conn_max_age=600
     )
 }
 
