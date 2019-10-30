f5a666dc0aee03358a6e4f05685d518b75218aa6
vantrong291 <vantrong291@gmail.com>
2019-10-30 10:58:11 +0700

    update logs for main server

--- saleor/site/migrations/0002_add_default_data.py
+++ saleor/site/migrations/0002_add_default_data.py
@@ -14,7 +14,7 @@ def create_default_site(apps, schema_editor):
         defaults={
             "name": "Saleor e-commerce",
             "header_text": "Test Saleor - a sample shop!",
-            "domain": "localhost:9090",
+            "domain": "localhost:8000",
         },
     )
 
